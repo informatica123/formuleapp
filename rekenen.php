eb339bf6cdf775680a618bf946f3187a361ceb4f
robert <fastreaction0@gmail.com>
2013-04-10 15:50:51 +0200

    versie2

---
+++ rekenen.php
@@ -0,0 +1,12 @@
+<?php
+	$getal1=$_POST["GetalVeld1"];
+	$getal2=$_POST["GetalVeld2"];
+	$som=$getal1+$getal2;
+	$kwadraat1=$getal1*$getal1;
+	$kwadraat2=$getal2*$getal2;
+	print("De som van $getal1 en $getal2 is $som <p> </p>");
+	print("Het kwadraat van $getal1 is $kwadraat1 <p> </p>");
+	print ("Het kwadraat van $getal2 is $kwadraat2 <p> </p>");
+	
+	?>
+	
